66292bee0ad34fdea57077379ecb9d5bd6f7ff0a
Abdul samad Pila valappil <apila-va@lab3r6s7.42abudhabi.ae>
2021-10-26 19:51:26 +0400

    ok

---
+++ get_next_line.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: apila-va <apila-va@42.abudhabi.ae>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/22 19:53:03 by apila-va          #+#    #+#             */
+/*   Updated: 2021/10/26 14:46:22 by apila-va         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <fcntl.h>
+
+#ifndef BUFFER_SIZE
+# define BUFFER_SIZE 2
+# endif
+
+char *get_next_line(int fd);
+char	*ft_strjoin(char const *s1, char const *s2);
+size_t	ft_strlen(const char *s);
+char	*ft_strchr(const char *str, int c);
+#endif
---
+++ get_next_line_utils.c
@@ -0,0 +1,56 @@
+#include "get_next_line.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*newstr;
+	char	*ptr;
+
+	printf("firs :%s\nsecond:%s\n" , s1,s2);
+	if (!s1 && !s2)
+		return (NULL);
+	newstr = malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+	if (newstr == NULL)
+		return (NULL);
+	ptr = newstr;
+	while (*s1)
+	{
+		*newstr = *s1;
+		s1++;
+		newstr++;
+	}
+
+	printf("joined_line:%s" , newstr);
+	while (*s2)
+	{
+		*newstr = *s2;
+		s2++;
+		newstr++;
+	}
+	*newstr = '\0';
+	return (ptr);
+}
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+char	*ft_strchr(const char *str, int c)
+{
+	while (*str)
+	{
+		if (*str == (char)c)
+			return ((char *)str);
+		str++;
+	}
+	if (c == '\0')
+		return ((char *)str);
+	return (0);
+}
